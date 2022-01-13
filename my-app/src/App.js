8c198af830a915477e6e0e3cfbf15ca60f6ecee9
Yami91001 <67729979+Yami91001@users.noreply.github.com>
2022-01-13 14:11:24 +0700

    React

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div class="personal_info">
+            <p>Phạm Anh Khoa</p>
+            <p>MSSV: 19120257</p>
+            <p>Lớp: 19CTT2</p>
+            <p>Trường Đại học Khoa học-Tự nhiên</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
