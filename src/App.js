0812e09f8daf7e9066f46763592c7fa69869f5e2
Yami91001 <67729979+Yami91001@users.noreply.github.com>
2022-01-14 15:49:35 +0700

    wrong

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 function App() {
   return (
+    wrong
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
